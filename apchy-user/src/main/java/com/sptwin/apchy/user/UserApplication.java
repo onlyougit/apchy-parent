883e8d768f5e55c83fbc6d5d71ab37ff50c6567a
王伟斌 <996041341@qq.com>
2018-08-08 17:52:47 +0800

    更换SpringBoot和SpringCloud版本

--- apchy-user/src/main/java/com/sptwin/apchy/user/UserApplication.java
+++ apchy-user/src/main/java/com/sptwin/apchy/user/UserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
--- apchy-user/src/main/java/com/sptwin/apchy/user/service/HystrixClient.java
+++ apchy-user/src/main/java/com/sptwin/apchy/user/service/HystrixClient.java
@@ -4,7 +4,7 @@ import com.sptwin.apchy.user.config.FeignConfig;
 import com.sptwin.apchy.user.service.impl.HystrixClientFallbackFactory;
 import feign.Param;
 import feign.RequestLine;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 
 //fallback和fallbackFactory只能有一个存在，fallbackFactory是fallback的扩展，能打印异常日志
 @FeignClient(name="apchy-order",configuration = FeignConfig.class,/* fallback =HystrixClientFallback.class,*/ fallbackFactory = HystrixClientFallbackFactory.class)
